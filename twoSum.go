66b9f7413a7d33a546dd8c350b539f65724c318c
Bryan Oliver <74172600+bryanoliverh@users.noreply.github.com>
2023-03-09 14:39:37 +0800

    Update twoSum.go

--- twoSum.go
+++ twoSum.go
@@ -3,17 +3,12 @@ func twoSum(array []int, target int) []int {
 seenNums := map[int]int{} 
 
 for i, num := range array { 
+  potentialMatch := target - num 
+  if j, ok := seenNums[potentialMatch]; ok { 
+     return []int{i, j} 
+  } 
 
-potentialMatch := target - num 
-
-if j, ok := seenNums[potentialMatch]; ok { 
-
-return []int{i, j} 
-
-} 
-
-seenNums[num] = i 
-
+  seenNums[num] = i 
 } 
 
 return []int{} 
