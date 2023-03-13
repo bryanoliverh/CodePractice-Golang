5e2743a50238129e73780de97be549fd788935bb
Bryan Oliver <74172600+bryanoliverh@users.noreply.github.com>
2023-03-13 17:13:56 +0800

    Update removeDuplicates.go

--- removeDuplicates.go
+++ removeDuplicates.go
@@ -1,5 +1,7 @@
 func removeDuplicates(nums []int) int {
     index := 1
+    // sort.Sort(sort.Reverse(sort.IntSlice(nums)))
+    sort.Sort(sort.IntSlice(nums))
     for i := 1; i < len(nums); i++ {
         if nums[i] != nums[i - 1] {
             nums[index] = nums[i]
