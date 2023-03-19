b4de7369e4e459efffb605040f6dcf320fcfb7af
Bryan Oliver <74172600+bryanoliverh@users.noreply.github.com>
2023-03-19 20:01:28 +0800

    Create interval.go

---
+++ interval.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"sort";
+	"fmt"
+)
+
+type Interval struct {
+    start int
+    end int
+}
+
+func mergeIntervals(intervals []Interval) []Interval {
+    if len(intervals) == 0 {
+        return []Interval{}
+    }
+    sort.Slice(intervals, func(i, j int) bool {
+        return intervals[i].start < intervals[j].start
+    })
+    merged := []Interval{intervals[0]}
+
+    for i := 1; i < len(intervals); i++ {
+        if intervals[i].start <= merged[len(merged)-1].end {
+            merged[len(merged)-1].end = max(merged[len(merged)-1].end, intervals[i].end)
+        } else {
+            merged = append(merged, intervals[i])
+        }
+    }
+
+    return merged
+}
+
+func max(a, b int) int {
+    if a > b {
+        return a
+    }
+    return b
+}
+
+
+func main(){
+	intervals := []Interval{
+    {1, 5},
+    {3, 7},
+    {4, 6},
+    {6, 8},
+}
+	merger :=(mergeIntervals(intervals))
+
+	for _, interval := range merger {
+    fmt.Printf("(%d, %d) ", interval.start, interval.end)
+}
+}
