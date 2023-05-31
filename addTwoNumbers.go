de28a2fd5417bab811a07d5ab795772f41e019b6
Bryan Oliver <74172600+bryanoliverh@users.noreply.github.com>
2023-05-31 10:37:01 +0800

    Create addTwoNumbers.go

---
+++ addTwoNumbers.go
@@ -0,0 +1,28 @@
+func addTwoNumbers (l1 *ListNode, l2 *ListNode) *ListNode{
+	dummy:= &ListNode{}
+	current := dummy
+	carry := 0
+
+	for l1 != nil || l2 != nil || carry !=0{
+		sum := carry
+		if l1 != nil{
+			sum+= l1.Val
+			l1=l1.Next
+		}
+
+		if l2 != nil{
+			sum+= l2.Val
+			l2=l2.Next
+		}
+
+		carry = sum/10
+		digit := sum%10
+
+		current.Next = &ListNode{Val: digit}
+		current = current.Next
+
+	}
+
+	return dummy.Next
+
+}
